9cee5d8f8f7ca2f1a99dfa8bb6ee3a88de9084f2
Beno-Jeffry-B <benojeffry08@gmail.com>
2025-06-21 02:02:24 +0530

    added content

--- first_react_app/src/index.js
+++ first_react_app/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<h1>hello world</h1>
+  <>
+    <h2>FUN FACTS ABOUT REACT</h2>
+    <ul>
+      <li> React Was Created at Facebook</li>
+      <li> React is Just the View Layer</li>
+      <li> JSX is Not Required (But It's Awesome)</li>
+      <li> Virtual DOM ≠ Real DOM</li>
+      <li>
+        React Powers Big Tech
+        <ol>
+          <li>Facebook (obviously!)</li>
+          <li>Instagram</li>
+          <li>Netflix</li>
+          <li>Airbnb</li>
+          <li>WhatsApp Web</li>
+        </ol>
+      </li>
+    </ul>
+  </>
+
 );
 
